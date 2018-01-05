671012b2d66ecce35c27d07435b687e31be98a95
John Blatchford <johnblatchford@users.noreply.github.com>
2018-01-04 20:06:26 -0800

    Update from channel map index to speaker position
    this allows the file to have the correct ext wav metadata for pro tools to detects the channel order correctly.

--- Film_to_SMPTE_order.py
+++ Film_to_SMPTE_order.py
@@ -11,19 +11,16 @@ Quick tool to remap a multi-channel RF64 wav file from:
 '''
 import ffmpy3
 from wavefile import WaveReader
+from argparse import RawTextHelpFormatter
 import os
-import sys
 import argparse
 
-# If script is run without arguments, display the usage.
-usage = "\nusage: Film_to_SMPTE_order.py -i path/to/folder/of/waves/ or /path/to/wav/file.wav\n"
-arguments = sys.argv[1:]
-if len(arguments) == 0:
-    print(usage)
-    sys.exit()
-
-# Set up args and the extension of files to process
-parser = argparse.ArgumentParser()
+# Set up args and the extension of files to process/
+parser = argparse.ArgumentParser(description=("Remap a multi-channel RF64 wav file from:\n\n"
+                                             "\t\tDOLBY SMPTE 5.1 Channel order (L R C LFE Ls Rs)\n"
+                                             "\t\t\t\tto\n"
+                                             "\t\tProTools & Film Channel order (L C R Ls Rs LFE)\n"),
+                                 formatter_class=RawTextHelpFormatter)
 parser.add_argument("-i", help="File/Folder for processing")
 args = parser.parse_args()
 # Initialize variables
@@ -41,8 +38,9 @@ if os.path.isdir(item):
                 out_file = os.path.join(item, file[:-4])
                 reorderFolder = ffmpy3.FFmpeg(
                     inputs={in_file: None},
-                    # set SMPTE order
-                    outputs={out_file + '_SMPTE.wav': "-rf64 auto -filter 'channelmap=0|2|1|5|3|4:5.1'"}
+                    # set Film order
+                    outputs={out_file + '_film.wav': "-rf64 auto -filter "
+                                                     "'channelmap=FL-FL|FR-FC|FC-FR|LFE-SR|SL-LFE|SR-SL'"}
                     )
                 reorderFolder.run()
             elif read_wav.channels == 8:
@@ -50,8 +48,9 @@ if os.path.isdir(item):
                 out_file = os.path.join(item, file[:-4])
                 reorderFolder = ffmpy3.FFmpeg(
                     inputs={in_file: None},
-                    # set SMPTE order
-                    outputs={out_file + '_SMPTE.wav': "-rf64 auto -filter 'channelmap=0|2|1|7|3|4|5|6:7.1'"}
+                    # set Film order
+                    outputs={out_file + '_film.wav': "-rf64 auto -filter "
+                                                     "'channelmap=FL-FL|FR-FC|FC-FR|LFE-BR|SL-LFE|SR-SL|BL-SR|BR-BL'"}
                     )
                 reorderFolder.run()
 
@@ -60,14 +59,16 @@ elif os.path.isfile(item):
     if read_wav.channels == 6:
         reorderFile = ffmpy3.FFmpeg(
             inputs={item: None},
-            # set SMPTE order
-            outputs={item[:-4] + '_SMPTE.wav': "-rf64 auto -filter 'channelmap=0|2|1|5|3|4:5.1'"}
+            # set Film order
+            outputs={item[:-4] + '_film.wav': "-rf64 auto -filter "
+                                              "'channelmap=FL-FL|FR-FC|FC-FR|LFE-SR|SL-LFE|SR-SL'"}
             )
         reorderFile.run()
     elif read_wav.channels == 8:
         reorderFile = ffmpy3.FFmpeg(
             inputs={item: None},
-            # set SMPTE order
-            outputs={item[:-4] + '_SMPTE.wav': "-rf64 auto -filter 'channelmap=0|2|1|7|3|4|5|6:7.1'"}
-        )
+            # set Film order
+            outputs={item[:-4] + '_film.wav': "-rf64 auto -filter "
+                                              "'channelmap=FL-FL|FR-FC|FC-FR|LFE-BR|SL-LFE|SR-SL|BL-SR|BR-BL'"}
+            )
         reorderFile.run()
